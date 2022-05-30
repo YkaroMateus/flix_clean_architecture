3fa3085fdbe34d9861a07694a86e23c93bf6e823
YkaroMateus <103946638+YkaroMateus@users.noreply.github.com>
2022-05-30 09:48:13 -0300

    update: most popular movies external layer

--- lib/app/module/movie_details/external/datasource/movie_details_datasource_implementation.dart
+++ lib/app/module/movie_details/external/datasource/movie_details_datasource_implementation.dart
@@ -1,10 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:flix_clean_ark/app/module/movie_details/external/datasource/settings/movie_details_settings.dart';
 import '../../../../../secrets.dart';
 import '../../../../core/configuration/config.dart';
 import '../../infrastructure/datasource/movie_details_datasource.dart';
 import '../../infrastructure/errors/movie_details_datasource_error.dart';
 import '../../infrastructure/models/movie_details_model.dart';
+import '../settings/movie_details_settings.dart';
 
 class MovieDetailsDatasourceImplementation implements MovieDetailsDatasource {
   final Dio dio;
