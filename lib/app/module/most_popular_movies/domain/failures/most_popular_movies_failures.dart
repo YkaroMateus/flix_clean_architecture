5b3a1918584a4b21aeb31e933afe2c2f63ec50fd
YkaroMateus <103946638+YkaroMateus@users.noreply.github.com>
2022-05-31 16:37:13 -0300

    delete: most popular movies domain layer

--- lib/app/module/most_popular_movies/domain/failures/most_popular_movies_failures.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flix_clean_ark/app/core/errors/failure.dart';
-
-abstract class MostPopularMoviesFailures implements Failure {}
--- lib/app/module/most_popular_movies/domain/repositories/most_popular_movies_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:flix_clean_ark/app/module/most_popular_movies/domain/entites/most_popular_movies.dart';
-import '../../../../core/errors/failure.dart';
-
-abstract class MostPopularMoviesRepository {
-  Future<Either<Failure, MostPopularMovies>> call();
-}
