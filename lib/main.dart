0fb36b609100988e0d90ff886d7f77a783b1d2fe
YkaroMateus <103946638+YkaroMateus@users.noreply.github.com>
2022-05-31 16:46:31 -0300

    update: main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:flix_clean_ark/app/module/app_module.dart';
-import 'package:flix_clean_ark/app/module/app_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'app/modules/app_module.dart';
+import 'app/modules/app_widget.dart';
 
 void main() {
   runApp(ModularApp(module: AppModule(), child: AppWidget()));
